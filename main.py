6207bd207bd7c46c3b69f21f71586e376631bfa9
GuFFy12 <49491499+GuFFy12@users.noreply.github.com>
2022-03-15 17:30:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -30,14 +30,13 @@ class Checker:
 
     def main(self):
         cls()
-        print(r"""
+        print(fr"""
    ___  _                     __  ______     __              _______           __                  ___ 
   / _ \(_)__ _______  _______/ / /_  __/__  / /_____ ___    / ___/ /  ___ ____/ /_____ ____  _  __|_  |
  / // / (_-</ __/ _ \/ __/ _  /   / / / _ \/  '_/ -_) _ \  / /__/ _ \/ -_) __/  '_/ -_) __/ | |/ / __/ 
 /____/_/___/\__/\___/_/  \_,_/   /_/  \___/_/\_\\__/_//_/  \___/_//_/\__/\__/_/\_\\__/_/    |___/____/ 
-                                                                                           by GuFFy_OwO                                                                                                     
+                                                                                           {Fore.CYAN}by GuFFy_OwO{Fore.RESET}                                                                                                    
 """)
-        time.sleep(2)
 
         print(f"{Fore.RESET}[{Fore.CYAN}1{Fore.RESET}] Enter token")
         print(f"{Fore.RESET}[{Fore.CYAN}2{Fore.RESET}] Check file")
@@ -64,8 +63,6 @@ class Checker:
                     for path in Path(token_file_name).rglob(search_type):
                         with open(path, "r", encoding="utf-8") as f:
                             tokens += f.read()
-
-
         else:
             fast_exit("Invalid Option.")
 
@@ -78,10 +75,10 @@ class Checker:
 
     def send_tokens(self):
         print()
-        print(f"Send tokens...")
+        print(f"Send tokens... Verification of 1000 tokens can take about 8 minutes.")
 
         try:
-            res = requests.post(self.url, json={"action": "checker", "data": self.tokens_parsed})
+            res = requests.post(self.url, timeout=600, json={"action": "checker", "data": self.tokens_parsed})
             if res.status_code != 200:
                 fast_exit("Status code is not 200. Something wrong with tokens...")
             elif res.status_code == 429:
@@ -97,7 +94,7 @@ class Checker:
                     for item in self.res["tokensInfo"][token_type]:
                         f.write("%s\n" % item)
 
-        with open('json_data.json', 'w', encoding='utf-8') as f:
+        with open('json_data.json', 'w') as f:
             json.dump(self.res, f, indent=4)
 
         fast_exit("Tokens saved!")

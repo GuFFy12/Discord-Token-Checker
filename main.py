85b251212636af499a8d0f3cc0e3ffe6cf1a94f4
GuFFy12 <49491499+GuFFy12@users.noreply.github.com>
2022-03-28 14:03:12 +0500

    Small fixes!

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def fast_exit(error):
 class Checker:
     def __init__(self):
         self.url = "https://lililil.xyz/checker"
-        self.tokens_unparsed = ""
+        self.tokens_not_parsed = ""
         self.tokens_parsed = []
         self.res = {}
 
@@ -36,7 +36,8 @@ class Checker:
   / _ \(_)__ _______  _______/ / /_  __/__  / /_____ ___    / ___/ /  ___ ____/ /_____ ____  _  __|_  |
  / // / (_-</ __/ _ \/ __/ _  /   / / / _ \/  '_/ -_) _ \  / /__/ _ \/ -_) __/  '_/ -_) __/ | |/ / __/ 
 /____/_/___/\__/\___/_/  \_,_/   /_/  \___/_/\_\\__/_//_/  \___/_//_/\__/\__/_/\_\\__/_/    |___/____/ 
-                                                                                           {Fore.CYAN}by GuFFy_OwO{Fore.RESET} 
+                                                                                           {Fore.CYAN}by GuFFy_OwO
+{Fore.RESET} 
 """)
 
         print(f"{Fore.RESET}[{Fore.CYAN}1{Fore.RESET}] Enter token")
@@ -46,30 +47,31 @@ class Checker:
         print()
 
         if "1" in check_type:
-            self.tokens_unparsed = input(
-                f"{Fore.CYAN}>{Fore.RESET}Enter tokens{Fore.CYAN}:{Fore.RESET} ")
+            self.tokens_not_parsed = input(f"{Fore.CYAN}>{Fore.RESET}Enter tokens{Fore.CYAN}:{Fore.RESET} ")
         elif "2" in check_type:
             token_file_name = input(
-                f"{Fore.CYAN}>{Fore.RESET}Enter the directory of the files or file in which are the unchecked tokens{Fore.CYAN}:{Fore.RESET} ")
+                f"{Fore.CYAN}>{Fore.RESET}Enter the directory of the files or file in which are the unchecked tokens"
+                f"{Fore.CYAN}:{Fore.RESET} "
+            )
             if not os.path.exists(token_file_name):
                 fast_exit(f"{token_file_name} directory not exist.")
 
             if os.path.isfile(token_file_name):
                 with open(token_file_name, "r", encoding="utf-8") as f:
-                    self.tokens_unparsed = f.read()
+                    self.tokens_not_parsed = f.read()
             else:
                 types = ["*.txt", "*.html", "*.json"]
                 for search_type in types:
                     for path in Path(token_file_name).rglob(search_type):
                         with open(path, "r", encoding="utf-8") as f:
-                            self.tokens_unparsed += f.read()
+                            self.tokens_not_parsed += f.read()
         else:
             fast_exit("Invalid Option.")
 
     def parse_tokens(self):
         pre_parsed = []
         for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
-            for token in re.findall(regex, self.tokens_unparsed):
+            for token in re.findall(regex, self.tokens_not_parsed):
                 pre_parsed.append(token)
         pre_parsed = list(dict.fromkeys(pre_parsed))
 
@@ -82,31 +84,39 @@ class Checker:
 
         if len(self.tokens_parsed) > 2000:
             fast_exit(
-                "The current API limit is 2000 tokens. Please sort the tokens by removing the cherished invalid tokens."
-                f" Amount of sorted tokens - {len(self.tokens_parsed)}."
+                f"The current API limit is {Fore.CYAN}2000{Fore.RESET} tokens. Please sort the tokens by removing the "
+                f"cherished invalid tokens. Amount of sorted tokens - {Fore.CYAN}{len(self.tokens_parsed)}{Fore.RESET}."
             )
         elif len(self.tokens_parsed) == 0:
-            print("Parser did not found tokens.")
+            fast_exit("Parser did not found tokens.")
 
     def send_tokens(self):
         print()
-        print(f"Send tokens... Verification of {len(self.tokens_parsed)} can take some time. 2000 tokens - 15 min.")
+        print(
+            f"Send tokens... Verification of {Fore.CYAN}{len(self.tokens_parsed)}{Fore.RESET} token can take some time."
+            f" {Fore.CYAN}2000{Fore.RESET} tokens - {Fore.CYAN}15{Fore.RESET} min."
+        )
 
         try:
             res = requests.post(self.url, json={"action": "checker", "data": self.tokens_parsed})
 
             if res.status_code == 429:
-                fast_exit(f"Too many tokens check, try after {res.headers['RateLimit-Reset']} seconds...")
+                fast_exit(
+                    f"Too many tokens check, try after "
+                    f"{Fore.CYAN}{res.headers['RateLimit-Reset']}{Fore.RESET} seconds..."
+                )
             elif res.status_code != 200:
-                fast_exit(f"Status code is {res.status_code}. {res.json()}")
+                fast_exit(f"Status code is {Fore.CYAN}{res.status_code}{Fore.RESET}. {res.json()}")
 
             self.res = res.json()
-        except:
-            fast_exit("An error occurred while trying to send the file to the server.")
+        except Exception as e:
+            fast_exit(f"An error occurred while trying to send the file to the server. {str(e)}")
 
     def save_res(self):
+        result = ""
         for token_type in self.res["tokensInfo"].keys():
             if self.res["tokensInfo"][token_type]:
+                result += f"{token_type} - {Fore.CYAN}{len(self.res['tokensInfo'][token_type])}{Fore.RESET}, "
                 with open(token_type + ".txt", "w") as f:
                     for item in self.res["tokensInfo"][token_type]:
                         f.write("%s\n" % item)
@@ -114,7 +124,7 @@ class Checker:
         with open("json_data.json", "w") as f:
             json.dump(self.res, f, indent=4)
 
-        fast_exit("Tokens saved!")
+        fast_exit(f"Tokens saved!\n\nStats: {result[:-3]}")
 
 
 if __name__ == "__main__":
